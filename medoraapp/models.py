21aece8561254c0f2ccee9343e72c634bb9c1601
ANGELA <angela.mbithe@student.moringaschool.com>
2022-01-17 10:19:25 +0300

    creation of models

--- medoraapp/models.py
+++ medoraapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+from datetime import datetime as dt
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_pic = models.ImageField(upload_to='images/', default='default.png')
+    bio = models.TextField(max_length=500, default="My Bio", blank=True)
+    contact = models.CharField(max_length = 10,blank =True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save_profile(self):
+        self.save()
+    
+    @classmethod
+    def search_profile(cls,username):
+        return cls.objects.fiter(user__username__icontains = username).all()
+
+class Vitals(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="vitals")
+    temperature = models.CharField(max_length = 10,blank =True)
+    pulserate = models.CharField(max_length = 10,blank =True)
+    bloodpressure = models.CharField(max_length = 10,blank =True)
+    symptoms = models.TextField(max_length=1000,blank=True)
+
+    def __str__(self):
+        return f'{self.user.username}'
+
+    def save_vitals(self):
+        self.save()
---
+++ medoraapp/urls.py
@@ -0,0 +1,11 @@
+from django import urls
+from django.urls import path
+from . import views
+
+urlpatterns =[
+    # path('register/', views.registerPage, name= 'register'),
+    # path('login/', views.loginPage, name= 'loginpage'),
+    # path('logout/',views.logoutpage,name='logout'),
+
+    path('',views.index, name = 'index'),
+    ]
